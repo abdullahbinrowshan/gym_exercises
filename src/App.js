d284d758cc3cb7c62b95c38a3bf15207f9fdd79b
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2023-04-21 12:28:10 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: ''
+    }
+])
+
+const App = () => {
+    return (
+        <Box width={`400px`}>
+            <RouterProvider router={router} />
+        </Box>
+    );
+};
 
 export default App;
