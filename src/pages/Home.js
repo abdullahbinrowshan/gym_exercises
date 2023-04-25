10b6541520d0874f6608d088492abc4ae06a3a57
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2023-04-25 11:44:30 +0600

    main exercises components is done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,22 @@
 import { Box } from '@mui/material';
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import HeroBanner from '../components/HeroBanner';
 import SearchExercises from '../components/SearchExercises';
 import Exercises from '../components/Exercises';
 
+export const CategoryContext = createContext('')
+
 const Home = () => {
+    const [bodyPart, setBodyPart] = useState('all');
+    const [exercises, setExercises] = useState([]);
     return (
-        <Box>
-            <HeroBanner />
-            <SearchExercises />
-            <Exercises />
-        </Box>
+        <CategoryContext.Provider value={{ bodyPart, setBodyPart, exercises, setExercises }}>
+            <Box>
+                <HeroBanner />
+                <SearchExercises />
+                <Exercises />
+            </Box>
+        </CategoryContext.Provider>
     );
 };
 
